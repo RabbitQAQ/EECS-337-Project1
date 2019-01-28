3751f2a926f0881cd4eb1a9c1fc02b85461d6304
charlesdai <email@charles_dai@qq.com>
2019-01-27 18:35:40 -0600

    mongodb complete - Jiawei

---
+++ readme.txt
@@ -0,0 +1,21 @@
+# update 01/27 -- Jiawei
+
+Mongodb related functions are completed
+I find we don't need a json parser because the json format can read directly into mongodb
+
+And I add 2 functions to Utils.py
+
+readJsonIntoDB(filepath, collectionName) can help you read json files into mongodb
+The collectionName can be "gg2013" or "gg2015" depending on the data you read
+
+readDBIntoTweetList(collectionName) can help you search db and convert the contents into a list of Tweet
+Also you need to specify the collectionName because we will have 2 MongoDB Collection(gg2013 and gg2015)
+
+If you need to install MongoDB, you can follow the instruction:
+https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x/
+
+Just use the default settings because we will put our db at /data/db
+Note that our db name is nlpTeamJJC(Jaieu, Jiawei and Chenbo), not local/config/test/admin
+Don't worry, just a note, simply execute readJsonIntoDB() is fine.
+
+If you use pycharm, you can search for a plugin called "Mongo Plugin", a GUI tool for MongoDB
---
+++ utils/Utils.py
@@ -0,0 +1,48 @@
+import json
+
+from pymongo import MongoClient
+
+from model.Tweet import Tweet
+from model.User import User
+
+# Read local json files into db
+def readJsonIntoDB(filepath, collectionName):
+    try:
+        with open(filepath) as input_file:
+            data = json.load(input_file)
+            client = MongoClient()
+            db = client["nlpTeamJJC"]
+            collection = db[collectionName]
+            collection.insert(data)
+    except:
+        print("Read Json Into DB: Operation Failed")
+    else:
+        print("Read Json Into DB: Operation Succeed")
+
+# Return a list of Tweet read from db
+def readDBIntoTweetList(collectionName):
+    res = []
+    client = MongoClient()
+    db = client["nlpTeamJJC"]
+    collection = db[collectionName]
+    # Get cursor from db to get all contents
+    cursor = collection.find({})
+    # Convert json-like contents to Tweet model
+    for item in cursor:
+        tmpTweet = Tweet(item['text'], item['timestamp_ms'], item['user']['id'], item['user']['screen_name'],
+                         item['id'])
+        res.append(tmpTweet)
+
+    # Return a list of Tweet
+    return res
+
+
+if __name__ == '__main__':
+    # Execute the following line only ONCE for each json file.
+    # If you are importing gg2013.json, the second parameter should be gg2013.
+    # If importing gg2015.json, then please change it to gg2015.
+    # gg2013 is much smaller and suitable for testing.
+    readJsonIntoDB('../data/gg2013.json', "gg2013")
+
+    # An example for read contents from db
+    tweetList = readDBIntoTweetList("gg2013")
